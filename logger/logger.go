69e0480ae67fdbd6d8593ebf5bae20836c71b7b0
Alexey Kovrizhkin <lekovr@gmail.com>
2022-12-05 05:27:07 +0300

    2 slashes via zerolog.CallerMarshalFunc

--- logger/logger.go
+++ logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zerologr"
@@ -21,21 +22,7 @@ func New(cfg Config, out io.Writer) logr.Logger {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
 	zerologr.NameFieldName = "logger"
 	zerologr.NameSeparator = "/"
-	/*	zerolog.CallerMarshalFunc = func(file string, line int) string {
-		short := file
-		slashes := 1
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				short = file[i+1:]
-				if slashes == 0 {
-					break
-				}
-				slashes--
-			}
-		}
-		file = short
-		return file + ":" + strconv.Itoa(line)
-	}*/
+	zerolog.CallerMarshalFunc = shortCallerPath
 	if out == nil {
 		if cfg.Destination == "" {
 			out = os.Stderr
@@ -54,6 +41,22 @@ func New(cfg Config, out io.Writer) logr.Logger {
 	var log logr.Logger = zerologr.New(&zl)
 	return log
 }
+
+func shortCallerPath(file string, line int) string {
+		short := file
+		slashes := 2
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				if slashes == 0 {
+					break
+				}
+				slashes--
+			}
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
 
 // NewContext calls logr.NewContext so ypu don't need to import logr for it.
 func NewContext(ctx context.Context, logger logr.Logger) context.Context {
