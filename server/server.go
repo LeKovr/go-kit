26c81d0780f69e8269add7870c79afda2ec21c84
Alexey Kovrizhkin <lekovr@gmail.com>
2026-02-17 02:44:19 +0300

    add: * config: ReadHeaderTimeout, IdleTimeout, AccessLog, UseETag * service: WithWorkers, WithNWorkers, WithHTTPWorkers
    fix:
    * server.TLSConfig
    * support for WriteTimeout == 0
    
    also:
    * WithAccessLog, WithETag methods moved to config vars

--- server/server.go
+++ server/server.go
@@ -5,13 +5,13 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"log/slog"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
@@ -20,14 +20,14 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-// TLSConfig holds TLS config options
+// TLSConfig holds TLS config options.
 type TLSConfig struct {
 	CertFile           string `long:"cert" description:"CertFile for serving HTTPS instead HTTP" env:"CERT"`
 	KeyFile            string `long:"key"  description:"KeyFile for serving HTTPS instead HTTP" env:"KEY"`
 	NoCheckCertificate bool   `long:"no-check" description:"disable tls certificate validation"`
 }
 
-// VersionResponseConfig
+// VersionResponseConfig holds settings for HTTP version response.
 type VersionResponseConfig struct {
 	Prefix string `long:"prefix" default:"/js/version.js" description:"URL for version response"`
 	Format string `long:"format" default:"document.addEventListener('DOMContentLoaded', () => { appVersion.innerText = '%s'; });\n" description:"Format string for version response"`
@@ -38,13 +38,17 @@ type VersionResponseConfig struct {
 type Config struct {
 	Listen string `long:"listen" default:":8080" description:"Addr and port which server listens at" env:"LISTEN"`
 
-	MaxHeaderBytes int           `long:"maxheader" description:"MaxHeaderBytes"`
-	ReadTimeout    time.Duration `long:"rto" default:"10s" description:"HTTP read timeout"`
-	WriteTimeout   time.Duration `long:"wto" default:"60s" description:"HTTP write timeout"`
-	GracePeriod    time.Duration `long:"grace" default:"10s" description:"Stop grace period"`
+	MaxHeaderBytes    int           `long:"maxheader" description:"MaxHeaderBytes"`
+	ReadTimeout       time.Duration `long:"rto" default:"10s" description:"HTTP read timeout"`
+	WriteTimeout      time.Duration `long:"wto" default:"60s" description:"HTTP write timeout, '0' means disable"`
+	ReadHeaderTimeout time.Duration `long:"rhto" default:"10s" description:"HTTP read header timeout"`
+	IdleTimeout       time.Duration `long:"ito" default:"10s" description:"HTTP idle timeout"`
+	GracePeriod       time.Duration `long:"grace" default:"10s" description:"Stop grace period"`
 
 	IPHeader   string `long:"ip_header" env:"IP_HEADER" default:"X-Real-IP" description:"HTTP Request Header for remote IP"`
 	UserHeader string `long:"user_header" env:"USER_HEADER" default:"X-Username" description:"HTTP Request Header for username"`
+	AccessLog  string `long:"access_log" env:"ACCESS_LOG" description:"HTTP access log filename (default: STDOUT, '-' means disable)"`
+	UseETag    bool   `long:"etag" env:"ETAG" description:"Add ETAG in HTTP response"`
 
 	TLS     TLSConfig             `group:"HTTPS Options"            namespace:"tls"  env-namespace:"TLS"`
 	Version VersionResponseConfig `group:"Version response Options" namespace:"vr"`
@@ -58,12 +62,18 @@ type Worker func(ctx context.Context) error
 
 // Service holds service attributes.
 type Service struct {
-	config     Config
-	listener   net.Listener
-	mux        *http.ServeMux
-	handlers   []Handler
-	onShutdown *Worker
+	config          Config
+	listener        net.Listener
+	server          *http.Server
+	mux             *http.ServeMux
+	handlers        []Handler
+	workers         []Worker
+	onShutdown      *Worker
+	accessLogWriter io.Writer
 }
+
+// AccessLogDisabled holds access_log value for access logging disabling.
+const AccessLogDisabled = "-"
 
 // New returns *Service.
 func New(cfg Config) *Service {
@@ -109,6 +119,9 @@ func (srv Service) ServeMuxWithHandlers() http.Handler {
 	for _, handler := range srv.handlers {
 		mux = handler(mux)
 	}
+	if srv.config.UseETag {
+		mux = etag.Handler(mux, false)
+	}
 	return mux
 }
 
@@ -123,29 +136,110 @@ func (srv *Service) WithShutdown(worker Worker) *Service {
 	return srv
 }
 
-// RunWorkers runs workers without HTTP servece.
-func (srv Service) RunWorkers(ctxParent context.Context, workers ...Worker) error {
-	ctx, stop := signal.NotifyContext(ctxParent, os.Interrupt, syscall.SIGTERM)
-	defer stop()
+// WithWorkers registers workers.
+func (srv *Service) WithWorkers(workers ...Worker) *Service {
+	srv.workers = append(srv.workers, workers...)
+	return srv
+}
+
+// WithNWorkers registers N copies of worker.
+func (srv *Service) WithNWorkers(n int, worker Worker) *Service {
+	for range n {
+		srv.workers = append(srv.workers, worker)
+	}
+	return srv
+}
+
+// WithHTTPWorkers registers HTTP workers.
+func (srv *Service) WithHTTPWorkers() *Service {
 
 	cfg := srv.config
+	//mux := srv.ServeMuxWithHandlers()
+
+	// Creating a normal HTTP server
+	server := &http.Server{
+		ReadTimeout:       cfg.ReadTimeout,
+		ReadHeaderTimeout: cfg.ReadHeaderTimeout,
+		IdleTimeout:       cfg.IdleTimeout,
+		MaxHeaderBytes:    cfg.MaxHeaderBytes,
+	}
+	if cfg.WriteTimeout != 0 {
+		server.WriteTimeout = cfg.WriteTimeout
+	}
+	if cfg.TLS.NoCheckCertificate {
+		server.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	workers := make([]Worker, 2)
+	if srv.config.TLS.CertFile != "" {
+		workers[0] = func(_ context.Context) error {
+			slog.Debug("Start HTTPS service")
+			return server.ServeTLS(srv.listener, srv.config.TLS.CertFile, srv.config.TLS.KeyFile)
+		}
+	} else {
+		workers[0] = func(_ context.Context) error {
+			slog.Debug("Start HTTP service")
+			return server.Serve(srv.listener)
+		}
+	}
+	workers[1] = func(ctx context.Context) error {
+		<-ctx.Done()
+		timedCtx, cancel := context.WithTimeout(ctx, cfg.GracePeriod)
+		defer cancel()
+		return server.Shutdown(timedCtx)
+	}
+	srv.server = server
+	srv.WithWorkers(workers...)
+	return srv
+}
+
+// Run runs HTTP(s) service and workers. HTTP Workers will be registered if none.
+func (srv *Service) Run(ctx context.Context, workers ...Worker) error {
+	cfg := srv.config
+	if srv.listener == nil {
+		slog.Debug("Start Listener", "addr", cfg.Listen)
+		listener, err := net.Listen("tcp", cfg.Listen)
+		if err != nil {
+			return err
+		}
+		srv.listener = listener
+	}
+	if srv.server == nil {
+		srv.WithHTTPWorkers()
+	}
+	server := srv.server
+	server.Handler = srv.ServeMuxWithHandlers() // Use aclual handlers list.
+	server.BaseContext = func(_ net.Listener) context.Context {
+		return ctx
+	}
+	if cfg.AccessLog != "" && cfg.AccessLog != AccessLogDisabled {
+		writer, err := os.OpenFile(cfg.AccessLog, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			return err
+		}
+		srv.accessLogWriter = writer
+		defer writer.Close()
+		server.Handler = srv.accessLogHandler(server.Handler)
+	}
+
+	return srv.WithWorkers(workers...).run(ctx)
+}
+
+// RunWorkers runs workers without HTTP service.
+func (srv *Service) RunWorkers(ctx context.Context, workers ...Worker) error {
+	return srv.WithWorkers(workers...).run(ctx)
+}
+
+func (srv *Service) run(ctxParent context.Context) error {
+	ctx, stop := signal.NotifyContext(ctxParent, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	// start servers
 	g, gCtx := errgroup.WithContext(ctx)
 	g.Go(func() error {
-		<-gCtx.Done()
-		slog.Debug("Shutdown")
-		stop()
-		timedCtx, cancel := context.WithTimeout(ctx, cfg.GracePeriod)
-		defer cancel()
-		var err error
-		if srv.onShutdown != nil {
-			w := *srv.onShutdown
-			err = w(timedCtx)
-		}
-		return err
+		return srv.shutdownWorker(gCtx)
 	})
-	for _, worker := range workers {
+	for _, worker := range srv.workers {
 		w := worker
 		g.Go(func() error {
 			return w(gCtx)
@@ -158,90 +252,47 @@ func (srv Service) RunWorkers(ctxParent context.Context, workers ...Worker) erro
 	return nil
 }
 
-// Run runs HTTP(s) service and workers.
-func (srv Service) Run(ctxParent context.Context, workers ...Worker) error {
-
-	cfg := srv.config
-	mux := srv.ServeMuxWithHandlers()
-	listener := srv.listener
-	if listener == nil {
-		var err error
-		slog.Debug("Start Listener", "addr", cfg.Listen)
-		if listener, err = net.Listen("tcp", cfg.Listen); err != nil {
-			return err
-		}
+func (srv *Service) shutdownWorker(ctx context.Context) error {
+	<-ctx.Done()
+	slog.Debug("Shutdown")
+	timedCtx, cancel := context.WithTimeout(context.Background(), srv.config.GracePeriod)
+	defer cancel()
+	var err error
+	if srv.onShutdown != nil {
+		w := *srv.onShutdown
+		err = w(timedCtx)
 	}
-	// Creating a normal HTTP server
-	server := &http.Server{
-		Handler: mux,
-		BaseContext: func(_ net.Listener) context.Context {
-			return ctxParent
-		},
-		ReadTimeout:    cfg.ReadTimeout,
-		WriteTimeout:   cfg.WriteTimeout,
-		MaxHeaderBytes: cfg.MaxHeaderBytes,
-	}
-	if cfg.TLS.NoCheckCertificate {
-		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	}
-
-	allWorkers := make([]Worker, 2)
-	if srv.config.TLS.CertFile != "" {
-		allWorkers[0] = func(_ context.Context) error {
-			slog.Debug("Start HTTPS service")
-			return server.ServeTLS(listener, srv.config.TLS.CertFile, srv.config.TLS.KeyFile)
-		}
-	} else {
-		allWorkers[0] = func(_ context.Context) error {
-			slog.Debug("Start HTTP service")
-			return server.Serve(listener)
-		}
-	}
-	allWorkers[1] = func(ctx context.Context) error {
-		<-ctx.Done()
-		timedCtx, cancel := context.WithTimeout(ctx, cfg.GracePeriod)
-		defer cancel()
-		return server.Shutdown(timedCtx)
-	}
-	allWorkers = append(allWorkers, workers...)
-	return srv.RunWorkers(ctxParent, allWorkers...)
+	return err
 }
 
 // WithAccessLog calculates estimate and prints HTTP request log.
-func (cfg Config) WithAccessLog(handler http.Handler) http.Handler {
+func (srv Service) accessLogHandler(handler http.Handler) http.Handler {
+	var writer io.Writer = os.Stdout
+	if srv.accessLogWriter != nil {
+		writer = srv.accessLogWriter
+	}
+	cfg := srv.config
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		m := httpsnoop.CaptureMetrics(handler, w, r)
 		ip := r.Header.Get(cfg.IPHeader)
 		if ip == "" {
-			if colonPos := strings.Index(r.RemoteAddr, ":"); colonPos == -1 {
-				ip = r.RemoteAddr
-			} else {
-				ip = r.RemoteAddr[:colonPos]
-			}
+			ip, _, _ = net.SplitHostPort(r.RemoteAddr)
 		}
 		user := r.Header.Get(cfg.UserHeader)
 		if user == "" {
 			user = "-"
 		}
-		fmt.Printf(`%s - %s [%s] "%s %s" %d %s %d %s%s`,
+		fmt.Fprintf(writer, `%s - %s [%s] "%s %s" %d %s %d %s%s`,
 			ip,
 			user,
 			time.Now().Format(time.DateTime),
 			r.Method,
-			r.URL,
+			r.URL.RequestURI(),
 			m.Code,
 			m.Duration,
 			m.Written,
 			r.Header.Get("Referer"),
 			"\n",
 		)
-
-	})
-}
-
-// WithETag adds ETAG to response.
-func (cfg Config) WithETag(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		etag.Handler(handler, false).ServeHTTP(w, r)
 	})
 }
--- server/server_test.go
+++ server/server_test.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"net"
@@ -108,12 +107,12 @@ func TestUseMiddleware(t *testing.T) {
 }
 
 func TestWithETag(t *testing.T) {
-	srv := New(Config{Listen: ":0"})
+	srv := New(Config{Listen: ":0", UseETag: true})
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("etag test"))
 	})
-	wrapped := srv.Config().WithETag(handler)
-	ts := httptest.NewServer(wrapped)
+	srv.ServeMux().HandleFunc("/", handler)
+	ts := httptest.NewServer(srv.ServeMuxWithHandlers())
 	defer ts.Close()
 
 	resp, err := http.Get(ts.URL)
@@ -125,32 +124,44 @@ func TestWithETag(t *testing.T) {
 		t.Fatal("ETag header missing")
 	}
 }
+
 func TestWithAccessLog(t *testing.T) {
-	srv := New(Config{Listen: ":0"})
+	file := filepath.Join(t.TempDir(), "access.log")
+	srv := New(Config{Listen: ":0", AccessLog: file})
+
+	// create a listener to capture address
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("listen: %v", err)
+	}
+	srv.WithListener(ln)
+	ctx, cancel := context.WithCancel(context.Background())
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("access log test"))
+		cancel()
 	})
-	wrapped := srv.Config().WithAccessLog(handler)
-	ts := httptest.NewServer(wrapped)
-	defer ts.Close()
+	srv.ServeMux().HandleFunc("/", handler)
 
-	old := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	resp, err := http.Get(ts.URL)
+	go func() {
+		if err := srv.Run(ctx); err != nil && !strings.Contains(err.Error(), "context canceled") {
+			t.Logf("Run error: %v", err)
+		}
+	}()
+	// give server time to start
+	time.Sleep(100 * time.Millisecond)
+	addr := ln.Addr().String()
+	resp, err := http.Get("http://" + addr + "/")
 	if err != nil {
 		t.Fatalf("GET: %v", err)
 	}
 	defer resp.Body.Close()
 
-	w.Close()
-	var buf bytes.Buffer
-	io.Copy(&buf, r) // Read from the pipe
-	os.Stdout = old  // Restore original stdout
-
-	if !strings.Contains(buf.String(), `"GET /" 200`) {
-		t.Fatalf("Log data header missing: %s", buf.String())
+	log, err := os.ReadFile(file)
+	if err != nil {
+		t.Fatalf("Read log: %v", err)
+	}
+	if !strings.Contains(string(log), `"GET /" 200`) {
+		t.Fatalf("Log data header missing: %s", string(log))
 
 	}
 
@@ -173,21 +184,23 @@ func TestWithShutdown(t *testing.T) {
 		t.Fatalf("worker not called")
 	}
 }
+
 func TestRunWorkers(t *testing.T) {
 	srv := New(Config{Listen: ":0"})
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel()
-	called := false
+	called := 0
 	worker := func(ctx context.Context) error {
-		called = true
+		called++
 		return nil
 	}
+	srv.WithNWorkers(2, worker)
 	err := srv.RunWorkers(ctx, worker)
 	if err != nil && !strings.Contains(err.Error(), "context canceled") {
 		t.Fatalf("RunWorkers returned error: %v", err)
 	}
-	if !called {
-		t.Fatalf("worker not called")
+	if called != 3 {
+		t.Fatalf("worker not called, want 3 got %d", called)
 	}
 }
 
